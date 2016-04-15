aef081b79009026d9070ad154f14cb96a88c5d05
kmggh <kenguyton@ezkbcbrdyyrbwpqmeemzvaunr.com>
2016-04-15 19:13:57 -0400

    Version 1.0.2 Fixed doc strings and int() python3 bug in first count number in a dataset.

--- count_sheep.py
+++ count_sheep.py
@@ -21,9 +21,9 @@ def get_args():
   parser.add_argument('-n', '--num', type=int,
                       help='Run for multiples of this number.')
   parser.add_argument('-v', '--verbose', default=False, action='store_true',
-                      help='Run for multiples of this number.')
+                      help='Output the set for each number * i.')
   parser.add_argument('--data', default=False, action='store_true',
-                      help='Process input data.')
+                      help='Process a sequence of input numbers.')
   return parser.parse_args()
 
 
@@ -40,7 +40,8 @@ def track_digits(accumulated_digits_seen, digit_sets):
     accumulated_digits_seen: set of str(int) digits.  Digits seen so far.
     digit_sets: function.  A generator that produces sets of digits seen.
   Yields:
-    A new set of digits seen for every update with a set from digit_sets.
+    A tuple of the int i, the current number, and thenew set of digits seen
+    for every update with a set from digit_sets.
   """
 
   for i, a_num, digits in digit_sets():
@@ -49,20 +50,31 @@ def track_digits(accumulated_digits_seen, digit_sets):
 
 
 def multiples(number):
-  """Generate multiples of a number."""
+  """Generate multiples of a number.
+
+  Yields:
+    A tuple pair of themultiple i and the number.
+  """
 
   for i in range(1, 1000):
     yield i, i * number
 
 
 def digits_seen_in_multiples(number):
-  """Return a simple generator for the digits seen in each multiple."""
+  """Return a simple generator for the digits seen in each multiple.
+
+  Yields:
+    The multiplier i, the number and the digits seen set.
+  """
 
   def generator():
+    """A simple generator of multiples."""
+
     for i, a_num in multiples(number):
       yield i, a_num, digits_seen(a_num)
 
   return generator
+
 
 def process_num(number, verbose):
   """Process a single number."""
@@ -83,13 +95,17 @@ def process_num(number, verbose):
       if accumulated_digits_seen == ALL_DIGITS:
         break
 
+
 def process_input_data_set():
   """Read a data set from stdin of a count and then numbers.
+
+  The first line of the input is a count of the the upcoming input numbers
+  then each line is one of those numbers to process.
 
   Output Case #n: <result>
   """
 
-  count = sys.stdin.readline().strip()
+  count = int(sys.stdin.readline().strip())
 
   case = 0
   for number_raw in sys.stdin:
@@ -98,11 +114,11 @@ def process_input_data_set():
       sys.exit(0)
 
     number = int(number_raw.strip())
-    
+
     if number == 0:
       print('Case #{0}: INSOMNIA'.format(case))
     else:
-      for i, a_num, accumulated_digits_seen in track_digits(
+      for unused_i, a_num, accumulated_digits_seen in track_digits(
           set([]), digits_seen_in_multiples(number)):
         if accumulated_digits_seen == ALL_DIGITS:
           break
@@ -110,7 +126,7 @@ def process_input_data_set():
 
 
 def main():
-  """Try these functions out."""
+  """Find the first multiple when all digits have been seen."""
 
   opts = get_args()
 
